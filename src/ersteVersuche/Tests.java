87c3437ea49a40582122df9dba882f80c64022d3
Simsal <simon_brueckl9@hotmail.com>
2016-03-10 14:14:27 +0100

    first commit

---
+++ src/ersteVersuche/Tests.java
@@ -0,0 +1,118 @@
+package ersteVersuche;
+
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class Tests {
+//	public static EV3UltrasonicSensor ultrasonicsensor_vorne = new EV3UltrasonicSensor(SensorPort.S1);
+//	public static EV3UltrasonicSensor ultrasonicsensor_hinten = new EV3UltrasonicSensor(SensorPort.S2);
+//	public static SampleProvider distanceMode = ultrasonicsensor_vorne.getDistanceMode();
+//	public static SampleProvider distanceMode_hinten = ultrasonicsensor_hinten.getDistanceMode();
+
+	public static RegulatedMotor motorLenkung = new EV3MediumRegulatedMotor(MotorPort.A);
+	public static RegulatedMotor motorAntrieb = new EV3LargeRegulatedMotor(MotorPort.B);
+	
+	public static int counter = 0;
+
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		while (Button.ESCAPE.isUp()) {
+
+//			if(Button.DOWN.isDown()){
+//				parkeEin();
+//			}
+			//68, 66,5
+			motorLenkung.rotate(-90);
+			motorAntrieb.rotate(-2500);
+			System.exit(0);
+////
+//			float[] sample = new float[distanceMode.sampleSize()];
+//			distanceMode.fetchSample(sample, 0);
+//			System.out.println(sample[0]);
+//			
+//			if (sample[0] < 0.5 && sample[0] != Float.NaN) {
+//				System.out.println("Sensor erkennt");
+//				counter = bewerteParkluecke(motorAntrieb.getTachoCount());
+//				if(counter == 2){
+//					
+//					parkeEin();
+//				}
+//			}
+		}
+	}
+
+	private static void normaleFahrt() {
+
+		motorAntrieb.backward();
+	}
+
+//	public static void parkeEin() {
+//		//
+////		motorAntrieb.rotate(-420);
+////		Delay.msDelay(200);
+//		// motorAntrieb.setSpeed(10);
+//		motorAntrieb.forward();
+//		motorLenkung.rotate(100);
+//		Delay.msDelay(1563);
+//		motorLenkung.rotate(-200);
+//		Delay.msDelay(1563);
+//		motorLenkung.rotate(100);
+//		motorAntrieb.flt();
+////		motorAntrieb.close();
+////		System.exit(0);
+//	}
+
+//	public static int bewerteParkluecke(int parklueckenStart) {
+//		System.out.println("parklueckenstart"+parklueckenStart);
+//		float[] sample = new float[distanceMode.sampleSize()];
+//		distanceMode.fetchSample(sample, 0);
+//		
+//		if (sample[0] < 0.5 && sample[0] != Float.NaN){
+//			int parklueckenEnde = motorAntrieb.getTachoCount();
+//			System.out.println("parklueckenEnde"+parklueckenEnde);
+//			if((parklueckenEnde-parklueckenStart)>200){
+//				return 2;
+//			}
+//			else {
+//				return 1;
+//			}
+//		}
+//		return 1;
+//	}
+//
+}
+
+// if(Button.LEFT.isDown()){
+// float[] sample = new float[distanceMode.sampleSize()];
+// distanceMode.fetchSample(sample, 0);
+// System.out.println(sample[0]);
+// }
+//
+// if(Button.RIGHT.isDown()){
+// float[] sample = new float[distanceMode_hinten.sampleSize()];
+// distanceMode_hinten.fetchSample(sample, 0);
+// System.out.println(sample[0]);
+// }
+
+// Delay.msDelay(5000);
+// motorLenkung.rotate(100);
+// Delay.msDelay(470);
+// motorLenkung.rotate(-200);
+// Delay.msDelay(470);
+// motorLenkung.rotate(100);
+// Delay.msDelay(5000);
+// System.out.println(motorAntrieb.getRotationSpeed());
+// motorAntrieb.flt();
+// float[] sample = new float[distanceMode.sampleSize()];
+// distanceMode.fetchSample(sample, 0);
+// System.out.printf("%.5f m\n", sample[0]);
+// LCD.drawString("Plugin Test", 0, 4);
+// Delay.msDelay(5000);
---
+++ src/ersteVersuche/ZustandsSteuerung.java
@@ -0,0 +1,123 @@
+package ersteVersuche;
+
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
+
+public class ZustandsSteuerung {
+	public static int zustandDesEinparkens = -1;
+	public static EV3UltrasonicSensor ultraschallSensorVorne =
+			new EV3UltrasonicSensor(SensorPort.S1);
+	// public static EV3UltrasonicSensor ultraschallSensorHinten = new
+	// EV3UltrasonicSensor(SensorPort.S2);
+	public static SampleProvider seitenAbstandNachRechts =
+			ultraschallSensorVorne.getDistanceMode();
+	// public static SampleProvider abstandNachHinten =
+	// ultraschallSensorHinten.getDistanceMode();
+	
+	public static EV3TouchSensor touchSensorOben = new EV3TouchSensor(SensorPort.S3);
+	public static SampleProvider einparkVorgangAuslöser = touchSensorOben.getTouchMode();
+
+	public static RegulatedMotor motorLenkung = new EV3MediumRegulatedMotor(MotorPort.A);
+	public static RegulatedMotor motorAntrieb = new EV3LargeRegulatedMotor(MotorPort.B);
+
+	static float parklueckenBeginn = 0;
+	static float parklueckenEnde = 0;
+	
+	public static void main(String[] args) {
+
+		
+		while (Button.ESCAPE.isUp()) {
+			motorAntrieb.backward();
+			float[] touchSensorObenMesswerte = new float[einparkVorgangAuslöser.sampleSize()];
+			einparkVorgangAuslöser.fetchSample(touchSensorObenMesswerte, 0);
+			
+			if(touchSensorObenMesswerte[0] == 1 && zustandDesEinparkens == -1){
+				wechselZustand(0, -motorAntrieb.getTachoCount());
+			}
+			
+			if (zustandDesEinparkens == 0) {
+				float[] seitenAbstandNachRechtsMesswerte =
+						new float[seitenAbstandNachRechts.sampleSize()];
+				seitenAbstandNachRechts.fetchSample(seitenAbstandNachRechtsMesswerte, 0);
+
+				if (seitenAbstandNachRechtsMesswerte[0] < 0.15 &&
+						seitenAbstandNachRechtsMesswerte[0] != Float.NaN) {
+					wechselZustand(1, -motorAntrieb.getTachoCount());
+					System.out.println(zustandDesEinparkens);
+				}
+				if (seitenAbstandNachRechtsMesswerte[0] > 0.15 |
+						seitenAbstandNachRechtsMesswerte[0] == Float.NaN) {
+					wechselZustand(2, -motorAntrieb.getTachoCount());
+					System.out.println(zustandDesEinparkens);
+
+				}
+
+			}
+			if (zustandDesEinparkens == 1) {
+				float[] seitenAbstandNachRechtsMesswerte =
+						new float[seitenAbstandNachRechts.sampleSize()];
+				seitenAbstandNachRechts.fetchSample(seitenAbstandNachRechtsMesswerte, 0);
+
+				if (seitenAbstandNachRechtsMesswerte[0] > 0.15 |
+						seitenAbstandNachRechtsMesswerte[0] == Float.NaN) {
+					wechselZustand(2, -motorAntrieb.getTachoCount());
+					System.out.println(zustandDesEinparkens);
+					System.out.println(motorAntrieb.getTachoCount());
+
+				}
+			}
+			if (zustandDesEinparkens == 2) {
+				float[] seitenAbstandNachRechtsMesswerte =
+						new float[seitenAbstandNachRechts.sampleSize()];
+				seitenAbstandNachRechts.fetchSample(seitenAbstandNachRechtsMesswerte, 0);
+
+				if (seitenAbstandNachRechtsMesswerte[0] < 0.15 &&
+						seitenAbstandNachRechtsMesswerte[0] != Float.NaN) {
+					parklueckenEnde = -motorAntrieb.getTachoCount();
+					System.out.println(motorAntrieb.getTachoCount());
+
+					if ((parklueckenEnde - parklueckenBeginn) > 540) {
+						wechselZustand(3, -motorAntrieb.getTachoCount());
+						System.out.println(zustandDesEinparkens);
+					} else {
+						wechselZustand(1, -motorAntrieb.getTachoCount());
+						System.out.println(zustandDesEinparkens);
+					}
+				}
+			}
+			if (zustandDesEinparkens == 3) {
+				parkeEin();
+
+			}
+		}
+	}
+
+	private static void parkeEin() {
+
+		motorAntrieb.flt();
+		motorAntrieb.rotate(-450);
+		motorLenkung.rotate(100);
+		motorAntrieb.rotate(650);
+
+		motorLenkung.rotate(-200);
+		motorAntrieb.rotate(580);
+
+		motorLenkung.rotate(100);
+
+		System.exit(0);
+	}
+
+	private static void wechselZustand(int zustand, float a) {
+		
+		zustandDesEinparkens = zustand;
+		parklueckenBeginn = a;
+	}
+
+}
